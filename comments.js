8dca7a171db6c4c97e449bdede095f4e9f7a7be7
Alberto Diaz Martin <adiazcan@hotmail.com>
2023-05-28 14:38:55 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,49 @@
+// Create web server
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var comments = [];
+
+var server = http.createServer(function(req, res) {
+    var parseObj = url.parse(req.url, true);
+
+    var pathname = parseObj.pathname;
+    if (pathname === '/') {
+        fs.readFile('./index.html', function(err, data) {
+            if (err) {
+                return res.end('404 Not Found.');
+            }
+            var htmlStr = template.render(data.toString(), {
+                comments: comments
+            });
+            res.end(htmlStr);
+        });
+    } else if (pathname.indexOf('/public/') === 0) {
+        fs.readFile('.' + pathname, function(err, data) {
+            if (err) {
+                return res.end('404 Not Found.');
+            }
+            res.end(data);
+        });
+    } else if (pathname === '/post') {
+        var comment = parseObj.query;
+        comment.dateTime = '2017-11-2 17:11:22';
+        comments.unshift(comment);
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+    } else {
+        fs.readFile('./404.html', function(err, data) {
+            if (err) {
+                return res.end('404 Not Found.');
+            }
+            res.end(data);
+        });
+    }
+});
+
+server.listen(3000, function() {
+    console.log('running...');
+});
